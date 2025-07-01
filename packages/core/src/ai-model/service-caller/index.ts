e02d7a6e9fd884054f9dcea8f4d55c02cacfe93a
RoLuoGan <15197446227@163.com>
2025-07-01 10:55:58 +0800

    fix(core): update ai model call input output

--- packages/core/src/ai-model/service-caller/index.ts
+++ packages/core/src/ai-model/service-caller/index.ts
@@ -250,8 +250,6 @@ export async function call(
   const debugCallInputOutput = getDebug('ai:original:call:input:output');
   const debugCallReasoning = getDebug('ai:original:call:reasoning');
 
-  debugCallInputOutput(`AI Model Call Input: ${JSON.stringify(messages)}`);
-
   const startTime = Date.now();
   const model = getModelName();
   let content: string | undefined;
@@ -304,7 +302,7 @@ export async function call(
     );
     content = result.choices[0].message.content!;
 
-    debugCallInputOutput(`AI Model Call Output: ${JSON.stringify(result)}`);
+    debugCallInputOutput(`AI Model Call Input Output: ${JSON.stringify({messages, result})}`);
 
     // 如果是vlm-ui-tars模型，输出reasoning_content调试日志
     const reasoningContent = (result.choices?.[0]?.message as any)?.reasoning_content;
